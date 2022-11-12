794c1efa8a611ad604a2cf648ee3fe1204e469dd
PacifistaPx0 <richieadigwe@gmail.com>
2022-11-12 03:12:04 +0100

    Error handling for invalid inputs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Python program to create a
 # GUI mark sheet using tkinter
+from tkinter import *
 import tkinter as tk
 import json
 
@@ -22,24 +23,43 @@ e2 = tk.Entry(master)
 e3 = tk.Entry(master)
 
 
+
+
 #function to append or write user details to json file
 #if json file is empty it creates an empty dictionary
 def savejson():
+    
     with open('users.json', 'r+') as f:        
         try:
             data = json.load(f)
         except json.JSONDecodeError:
             data = {}
             data['students']=[]
+    
+    try:
+        data['students'].append({
+            'name':e1.get(),
+            'matric':int(e2.get()),
+            'score':int(e3.get())
+        })
+    except ValueError:
+        return tk.Label(master, text="   invalid input    ").grid(row=1, column=4)
+    
+    #error handling
+    if len(e2.get()) != 9:
+        return tk.Label(master, text="  invalid matric  ").grid(row=1, column=4)
+    if int(e3.get()) < 0 or int(e3.get()) > 100:
+        return tk.Label(master, text="   invalid score    ").grid(row=1, column=4)
 
-    data['students'].append({
-        'name':e1.get(),
-        'matric':e2.get(),
-        'score':int(e3.get())
-    }) 
-
+    for i in data['students']:
+        if int(e2.get()) == i['matric']:
+            return tk.Label(master, text="   user exists   ").grid(row=1, column=4)
+    #write user into file
     with open("users.json", "w") as f:
         json.dump(data, f, indent=4)
+    
+    tk.Label(master, text="  succesful input  ").grid(row=1, column=4)
+    
      
 #function to display average of the entire score
 def displayAvg():
@@ -53,20 +73,22 @@ def displayAvg():
         except json.JSONDecodeError:
             return tk.Label(master, text="no data").grid(row=20, column=5)
     
-    
-    
+    #widget for average score
     tk.Label(master, text=str(avg/len(data['students']))).grid(row=20, column=5)
 
+#function to display max score 
 def displayMax():
-    max = 0
+    allvalues = []
     with open('users.json', 'r+') as f:
         try:
             data = json.load(f)
+            for i in data['students']:
+                allvalues += [i['score']]
         except json.JSONDecodeError:
             return tk.Label(master, text="no data").grid(row=22, column=5)
 
 
-    tk.Label(master, text=str(max)).grid(row=22, column=5)
+    tk.Label(master, text=str(max(allvalues))).grid(row=22, column=5)
  
     
 # label to enter name
@@ -79,7 +101,7 @@ tk.Label(master, text="Matric Number").grid(row=0, column=3)
 tk.Label(master, text="Score").grid(row=1, column=0)
  
 
-# taking entries of name, reg, roll number respectively
+# taking entries of name, matric number and score respectively
 e1=tk.Entry(master)
 e2=tk.Entry(master)
 e3=tk.Entry(master)
@@ -89,15 +111,15 @@ e1.grid(row=0, column=1)
 e2.grid(row=0, column=4)
 e3.grid(row=1, column=1)
   
-# button to display all the calculated credit scores and sgpa
+# button to save new students in json file
 button1=tk.Button(master, text="submit", bg="green", command=savejson)
 button1.grid(row=8, column=1)
   
-# button to display all the calculated credit scores and sgpa
+# button to display the average score
 button2=tk.Button(master, text="average", bg="green", command=displayAvg)
 button2.grid(row=20, column=4)
 
-# button to display all the calculated credit scores and sgpa
+# button to display the max score
 button3=tk.Button(master, text="max score", bg="green", command=displayMax)
 button3.grid(row=22, column=4)
   
